b4fff3a24f4d5270e53f6b5b72186ef2ef9b1a56
GitHub Action <action@github.com>
2025-02-13 05:01:45 +0000

    Initialize template repository proper
    Set package name to com.github.bunnythewolf.eschataddons
    Set project name to EschatAddons
    Set modid to eschataddons

--- src/main/java/com/github/bunnythewolf/eschataddons/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/bunnythewolf/eschataddons/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.bunnythewolf.eschataddons.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
